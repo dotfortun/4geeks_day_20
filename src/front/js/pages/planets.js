6f27c5b7570b9ce25dae7131bc5e62fbbd79f7f2
dotfortun <shane.bell.dev@gmail.com>
2022-06-22 23:44:46 +0000

    Fixed missing key.

--- src/front/js/pages/planets.js
+++ src/front/js/pages/planets.js
@@ -13,9 +13,10 @@ const Planets = () => {
     <div className="container text-light bg-dark">
       <div className="row">
         <div className="col-8 offset-2 d-flex flex-row flex-wrap justify-content-evenly">
-          {store.planets.map((planet) => {
+          {store.planets.map((planet, idx) => {
             return (
               <Card
+                key={idx}
                 title={planet.name}
                 text={`${planet.name} is a planet with a gravity of ${planet.gravity}, and it has a ${planet.climate} climate.`}
               />
